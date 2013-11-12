0bcc4348b48c280f3180d19bd3edb24ac5c29132
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-11 22:41:00 -0800

    Trying to find the recursive loop that walks through

--- run.rb
+++ run.rb
@@ -1,47 +1,60 @@
 class String
-    # Does what it says, matches single characters in strings
-    def matchCharacters(match, start=0, limit=nil)
-        matches = []
-        # look at each character and return it's index if it matches the match
-        # TODO re-write this so it makes more sense :p
-        (start..(self.length || limit)).each do |i|
-            matches << i if match == self[i, 1]
-        end
-        matches
+  # Does what it says, matches single characters in strings
+  def matchCharacters(match, start=0, limit=nil)
+    matches = []
+    # look at each character and return it's index if it matches the match
+    # TODO re-write this so it makes more sense :p
+    (start..(self.length || limit)).each do |i|
+      matches << i if match == self[i, 1]
+    end
+    matches
+  end
+    
+  def scanForSquares(character, index, limit)
+    matches = []
+    # Get matching characters for the initial_character, ignore the initial_character
+    # These individually represent square subsequences, however we are going to use them to "grow"
+    # longer subsequences
+    matching_indices = self.matchCharacters(character, index, limit)
+    matching_indices.each { |o| puts "#{character}:" + (' '*(index-1)) + character + (' '*(o-index)) + character}
+
+    # Example, if we have aabababaa
+    # The index could be 0, i.e. "a"
+    # Running the above will find the matches [1, 3, 5, 7, 8]
+
+    # For each other "half" of the square's start i.e. [1, 3, 5, 7, 8]
+    matching_indices.each do |i|
+
+      matches << self[i]
+      # This is where the recursing should happen
+      # From index character + 1, until the matching half's index
+      (index..i).each do |j|
+        puts '-'
+        self.scanForSquares(self[j], j+1, i)
+      end
     end
     
-    def scanForSquares(index)
-        matches = []
-        # Get matching characters for the initial_character, ignore the initial_character
-        matching_indices = self.matchCharacters(self[index], index+1)
-        
-        # Example, if we have aabababaa
-        # The index could be 0, i.e. "a"
-        # Running the above will find the matches [1, 3, 5, 7, 8]
-
-        # For each other square(b) start i.e. [1, 3, 5, 7, 8]
-        matching_indices.each do |i|
-            # From init_character + 1, until the matching half's init
-            (index + 1..i).each do |j|
-                self[index+1, j].matchCharacters(self[j], j)
-            end
-        end
-        
-        # matches
-    end
+    matches
+  end
 
 end
 
 def testCase input
-    matches = []
-    input.length.times do |i|
-        input.scanForSquares(i)
-        # matches <<
-    end
+  matches = {}
+
+  puts " :#{input}"
+
+  input.length.times do |i|
+    # puts input[i]
+    #matches[input[i]] = 
+    input.scanForSquares(input[i], i + 1, input.length)
+    puts "-------------------"
+    # puts
+  end
 end
 
 test_cases = gets.strip.to_i
 
 test_cases.times do
-    testCase(gets.strip)
+  testCase(gets.strip)
 end
