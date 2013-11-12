beeee218b4a4fa41a18ce5d24c1af65ad39cd666
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-12 01:08:36 -0800

    Almost using same recurser for all

--- run.rb
+++ run.rb
@@ -1,55 +1,62 @@
 class String
   # Does what it says, matches single characters in strings
-  def matchCharacters(match, start=0, limit=nil)
+  def matchCharacters(match, start=0)
     matches = []
     # look at each character and return it's index if it matches the match
     # TODO re-write this so it makes more sense :p
-    (start..(self.length || limit)).each do |i|
+    (start..self.length).each do |i|
       matches << i if match == self[i, 1]
     end
     matches
   end
-    
-  def scanForSquares(character, index, limit)
-    matches = []
+
+  def scanSquare(matches, character, left_start_index, right_start_index=nil)
+
+    # matches = []
     # Get matching characters for the initial_character, ignore the initial_character
     # These individually represent square subsequences, however we are going to use them to "grow"
     # longer subsequences
-    matching_indices = self.matchCharacters(character, index, limit)
-    matching_indices.each { |o| puts "#{character}:" + (' '*(index-1)) + character + (' '*(o-index)) + character}
+    right_indices = self.matchCharacters(character, (right_start_index || left_start_index) + 1)
 
     # Example, if we have aabababaa
-    # The index could be 0, i.e. "a"
+    # The left_start_index could be 0, i.e. "a"
     # Running the above will find the matches [1, 3, 5, 7, 8]
 
-    # For each other "half" of the square's start i.e. [1, 3, 5, 7, 8]
-    matching_indices.each do |i|
+    # Right hand is empty
+    if right_indices.empty?
+      return matches
+    end
 
-      matches << self[i]
-      # This is where the recursing should happen
-      # From index character + 1, until the matching half's index
-      (index..i).each do |j|
-        puts '-'
-        self.scanForSquares(self[j], j+1, i)
+    # For each other "half" of the square's start i.e. [1, 3, 5, 7, 8]
+    right_indices.each do |right_index|
+      # We didn't (but now do) have no right starts, in this case we wanna begin the subwalk
+      if right_start_index.nil?
+        # Recurse here? Omg omg omg
+        (left_start_index..right_index).each do |left_index|
+          matches << self.scanSquare(matches, self[left_index], left_start_index, right_index)
+        end
+      
+      else
+        left_index = left_start_index + 1 
+        if left_index < right_start_index - 1
+          return self.scanSquare(matches, self[left_index], left_index, right_index)
+        else
+          # Left hand is empty
+          return character     
+        end
       end
+
     end
     
-    matches
+    # matches
   end
 
 end
 
 def testCase input
-  matches = {}
-
-  puts " :#{input}"
-
-  input.length.times do |i|
-    # puts input[i]
-    #matches[input[i]] = 
-    input.scanForSquares(input[i], i + 1, input.length)
-    puts "-------------------"
-    # puts
+  input.length.times do |i| 
+    print input.scanSquare([], input[i], i)
+    puts
   end
 end
 
